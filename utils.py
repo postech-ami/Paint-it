c32b63c58ee9a0e43bf11530924e8e389feaac9e
Youwang-Kim <youwang.kim@postech.ac.kr>
2024-04-04 17:19:25 +0900

    added missing function: sample_view_obj in utils.py

--- utils.py
+++ utils.py
@@ -14,6 +14,61 @@ tensor_to_img = T.ToPILImage()
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
+def sample_view_obj(n_view, cam_radius, res=[512, 512], cam_near_far=[0.1, 1000.0], spp=1, is_face=False):
+    iter_res = res
+    fovy = np.deg2rad(45)
+    proj_mtx = util.perspective(fovy, iter_res[1] / iter_res[0], cam_near_far[0], cam_near_far[1])
+
+    # Random rotation/translation matrix for optimization.
+    mv_list, mvp_list, campos_list, direction_list = [], [], [], []
+    for view_i in range(n_view):
+        if view_i == 0:
+            angle_x = 0.0  # elevation
+            angle_y = 0.0  # azimuth
+        else:
+            angle_x = np.random.uniform(-np.pi / 3, np.pi / 3)
+            angle_y = np.random.uniform(0, 2 * np.pi)
+
+        # direction
+        # 0 = front, 1 = side, 2 = back, 3 = overhead
+        if angle_x < -np.pi / 4:
+            direction = 3
+        else:
+            if 0 <= angle_y <= np.pi / 4 or angle_y > 7 * np.pi / 4:
+                direction = 0
+            elif np.pi / 4 < angle_y <= 3 * np.pi / 4:
+                direction = 1
+            elif 3 * np.pi / 4 < angle_y <= 5 * np.pi / 4:
+                direction = 2
+            elif 5 * np.pi / 4 < angle_y <= 7 * np.pi / 4:
+                direction = 1
+
+        # for object, hard to tell front, back. so, perform prompt augment for only overhead view
+        # If the results do not look good, you may use this direction prompts.
+        # if angle_x < -np.pi / 4:
+        #     direction = 1
+        # else:
+        #     direction = 0
+
+        mv = util.translate(0, 0, -cam_radius) @ (util.rotate_x(angle_x) @ util.rotate_y(angle_y))
+        mvp = proj_mtx @ mv
+        campos = torch.linalg.inv(mv)[:3, 3]
+        mv_list.append(mv[None, ...].cuda())
+        mvp_list.append(mvp[None, ...].cuda())
+        campos_list.append(campos[None, ...].cuda())
+        direction_list.append(direction)
+
+    cam = {
+        'mv': torch.cat(mv_list, dim=0),
+        'mvp': torch.cat(mvp_list, dim=0),
+        'campos': torch.cat(campos_list, dim=0),
+        'direction': np.array(direction_list, dtype=np.int32),
+        'resolution': iter_res,
+        'spp': spp
+    }
+    return cam
+
+
 def sample_circle_view(n_view, elev, cam_radius, res=[512, 512], cam_near_far=[0.1, 1000.0], spp=1):
     iter_res = res
     fovy = np.deg2rad(45)
